a4a7634c518d51b7910de28386719bc7a8b80031
Nicklas Pape <nicklas@trendsales.dk>
2025-01-29 12:55:58 +0100

    Initial Commit

---
+++ swiftyhub/swiftyhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftyhubApp.swift
+//  swiftyhub
+//
+//  Created by Nicklas Skov Pape on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftyhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
