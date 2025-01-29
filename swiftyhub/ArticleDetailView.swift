7b4e6d94d2e32c47b719ed419eb62e933097c7cb
Nicklas Pape <nicklas@trendsales.dk>
2025-01-29 21:58:26 +0100

    Test

---
+++ swiftyhub/ArticleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailView.swift
+//  swiftyhub
+//
+//  Created by Nicklas Skov Pape on 29/01/2025.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleDetailView()
+}
---
+++ swiftyhub/ShimmerView.swift
@@ -0,0 +1,18 @@
+//
+//  ShimmerView.swift
+//  swiftyhub
+//
+//  Created by Nicklas Skov Pape on 29/01/2025.
+//
+
+import SwiftUI
+
+struct ShimmerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShimmerView()
+}
